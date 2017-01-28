d6012304ad8379f8a67692542b0f5909c3c61fe6
chaynes2019 <chaynes2019@gmail.com>
2017-01-28 15:13:58 -0500

    Finished Intake Subsystem integration

--- FRC3260_2016_gearShifter/src/org/usfirst/frc3620/FRC3260_2016_gearShifter/commands/DriveCmd.java
+++ FRC3260_2016_gearShifter/src/org/usfirst/frc3620/FRC3260_2016_gearShifter/commands/DriveCmd.java
@@ -54,10 +54,12 @@ public class DriveCmd extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	
     }
 }
